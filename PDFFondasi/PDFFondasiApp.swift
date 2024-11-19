db7cabe6af0f4ae39673ef8b583b1418bdb4e4ac
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-11-19 15:56:06 +0700

    Initial Commit

---
+++ PDFFondasi/PDFFondasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFFondasiApp.swift
+//  PDFFondasi
+//
+//  Created by Dason Tiovino on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PDFFondasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PDFFondasiTests/PDFFondasiTests.swift
@@ -0,0 +1,17 @@
+//
+//  PDFFondasiTests.swift
+//  PDFFondasiTests
+//
+//  Created by Dason Tiovino on 19/11/24.
+//
+
+import Testing
+@testable import PDFFondasi
+
+struct PDFFondasiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
